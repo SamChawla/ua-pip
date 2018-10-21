ff89e32983374b7478f6469cd97f675277d076f4
Sam <sam.chawla26@gmail.com>
2018-10-21 22:44:20 +0530

    Part-I programs added

---
+++ basic_io.py
@@ -0,0 +1,11 @@
+# This is a comment
+
+import sys
+
+def hello():
+    ''' My First Function '''
+    name = input("Enter your name: ")
+    print("Hello ",name , "!!!" )
+
+# Calling function
+hello()
